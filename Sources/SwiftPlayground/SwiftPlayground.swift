c86f7db5ae9fcae6b2f9b64e8c42409d59482e7c
valentinelelez-arch <valentine.lelez@student.onslow.school.nz>
2026-05-14 02:09:00 +0000

    May 14, 2026, 2:09 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -9,10 +9,22 @@ struct SwiftPlayground {
 var gymBadges = ["Boulder", "Cascade", "Thunder"]
 gymBadges.append("Rainbow")
 print(gymBadges)
+
 // .insert()
 var months = ["Jan", "Feb", "Apr"]
 months.insert("Mar", at: 2)
 print(months)
+
+// .remove()
+var os = ["Windows", "macOS", "BeOS", "Linux"]
+// Use this
+os.remove(at: 2)
+// Or use this
+if let index = os.firstIndex(of: "BeOS") {
+os.remove(at: index)
+}
+print(os)
+
 
     }
 }
